87055c2ea705b3b10add498b704128384979df9d
SerhiiMaletych <maitrewop@gmail.com>
2021-03-07 21:48:14 +0200

    12 - add html page for credit

--- src/main/java/money/calculator/MoneyCalculator/controller/CreditController.java
+++ src/main/java/money/calculator/MoneyCalculator/controller/CreditController.java
@@ -1,25 +1,32 @@
 package money.calculator.MoneyCalculator.controller;
 
 import money.calculator.MoneyCalculator.entity.Credit;
-import money.calculator.MoneyCalculator.entity.Deposit;
 import money.calculator.MoneyCalculator.service.CreditService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CreditController {
 
+    Credit credit = new Credit();
     private final CreditService creditService;
 
     public CreditController(CreditService creditService) {
         this.creditService = creditService;
     }
 
+    @RequestMapping(value="/credit")
+    public String creditMenu(Model model) {
+        model.addAttribute("credit", credit);
+        return "credit/credit-menu";
+    }
+
 
     @PostMapping(value = "/credit", params = "clear")
-    public String clear(@ModelAttribute("deposit") Credit credit , Model model) {
+    public String clear(@ModelAttribute("deposit") Credit credit, Model model) {
         model.addAttribute("credit", creditService.clear(credit));
         model.addAttribute("result", 0);
         return "credit/credit-menu";
