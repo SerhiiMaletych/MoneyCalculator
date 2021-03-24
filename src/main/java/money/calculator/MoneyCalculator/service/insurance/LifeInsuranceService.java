7a1dfc10f2662404adf9e5dfb0a3a24bd2406fac
SerhiiMaletych <maitrewop@gmail.com>
2021-03-24 22:54:40 +0200

    37 - add all needed methods for LifeInsuranceService, some UI changes

--- src/main/java/money/calculator/MoneyCalculator/service/insurance/LifeInsuranceService.java
+++ src/main/java/money/calculator/MoneyCalculator/service/insurance/LifeInsuranceService.java
@@ -1,10 +1,8 @@
 package money.calculator.MoneyCalculator.service.insurance;
 
-import money.calculator.MoneyCalculator.entity.insurance.Car;
 import money.calculator.MoneyCalculator.entity.insurance.Life;
 import money.calculator.MoneyCalculator.model.Employed;
 import money.calculator.MoneyCalculator.model.Result;
-import money.calculator.MoneyCalculator.model.insurance.CarModel;
 import money.calculator.MoneyCalculator.model.insurance.Job;
 import money.calculator.MoneyCalculator.model.insurance.ProSport;
 import money.calculator.MoneyCalculator.model.insurance.Sickness;
@@ -20,66 +18,97 @@ public class LifeInsuranceService {
     private double proSportMultiplier;
 
 
-
-    public double calculateLifeInsurancePayment() {
+    public double calculateLifeInsurancePayment(Life life) {
+        double basePayment = 100;
+        findBodyMultiplier(life);
+        findAgeMultiplier(life);
+        findAmateurSportMultiplier(life);
+        findSicknessMultiplier(life);
+        findJobMultiplier(life);
+        findProSportMultiplier(life);
+        return basePayment * bodyMultiplier * ageMultiplier * jobMultiplier * sicknessMultiplier *
+                amateurSportMultiplier * proSportMultiplier;
 
     }
 
     public double findProSportMultiplier(Life life) {
-
+        if (life.getProSport() == ProSport.NONE || life.getProSport() == ProSport.CHESS) {
+            return proSportMultiplier = 1;
+        } else if (life.getProSport() == ProSport.ATHLETICS || life.getProSport() == ProSport.ARCHERY ||
+                life.getProSport() == ProSport.BADMINTON || life.getProSport() == ProSport.GOLF ||
+                life.getProSport() == ProSport.RUNNING) {
+            return proSportMultiplier = 1.2;
+        } else if (life.getProSport() == ProSport.BASKETBALL || life.getProSport() == ProSport.DIVING ||
+                life.getProSport() == ProSport.FOOTBALL || life.getProSport() == ProSport.GYMNASTICS ||
+                life.getProSport() == ProSport.SKIING || life.getProSport() == ProSport.TENNIS ||
+                life.getProSport() == ProSport.TRIATHLON) {
+            return proSportMultiplier = 1.4;
+        } else if (life.getProSport() == ProSport.BOXING || life.getProSport() == ProSport.HOCKEY ||
+                life.getProSport() == ProSport.FENCING || life.getProSport() == ProSport.KARATE ||
+                life.getProSport() == ProSport.RUGBY || life.getProSport() == ProSport.POWERLIFTING ||
+                life.getProSport() == ProSport.WEIGHTLIFTING) {
+            return proSportMultiplier = 1.6;
+        } else if (life.getProSport() == ProSport.CROSSFIT) {
+            return proSportMultiplier = 1.7;
+        } else return 0;
     }
 
     public double findJobMultiplier(Life life) {
-
+        if (life.getJob() == Job.NONE || life.getJob() == Job.ARCHITECT || life.getJob() == Job.ACCOUNTANT ||
+                life.getJob() == Job.AUTHOR || life.getJob() == Job.HAIRDRESSER || life.getJob() == Job.LAWYER
+                || life.getJob() == Job.MODEL || life.getJob() == Job.TEACHER || life.getJob() == Job.WAITER) {
+            return jobMultiplier = 1;
+        } else if (life.getJob() == Job.ACTOR || life.getJob() == Job.DOCTOR || life.getJob() == Job.CLEANER
+                || life.getJob() == Job.FARMER || life.getJob() == Job.BAKER) {
+            return jobMultiplier = 1.2;
+        } else if (life.getJob() == Job.BUTCHER || life.getJob() == Job.DRIVER || life.getJob() == Job.FACTORY_WORKER
+                || life.getJob() == Job.MECHANIC) {
+            return jobMultiplier = 1.3;
+        } else if (life.getJob() == Job.BUILDER || life.getJob() == Job.ELECTRICIAN) {
+            return jobMultiplier = 1.4;
+        } else if (life.getJob() == Job.FIREFIGHTER || life.getJob() == Job.POLICEMAN) {
+            return jobMultiplier = 1.5;
+        } else if (life.getJob() == Job.PILOT || life.getJob() == Job.POLITICIAN || life.getJob() == Job.SOLDIER) {
+            return jobMultiplier = 1.7;
+        } else return 0;
     }
 
     public double findSicknessMultiplier(Life life) {
         if (life.getSickness() == Sickness.NONE) {
             return sicknessMultiplier = 1;
-        }
-        else if(life.getSickness()==Sickness.HEADACHE) {
+        } else if (life.getSickness() == Sickness.HEADACHE) {
             return sicknessMultiplier = 1.1;
-        }
-        else if(life.getSickness()==Sickness.DIABETES || life.getSickness()==Sickness.DIARRHEAL) {
-            return  sicknessMultiplier = 1.2;
-        }
-        else if(life.getSickness()==Sickness.PULMONARY || life.getSickness()==Sickness.DEMENTIAS) {
+        } else if (life.getSickness() == Sickness.DIABETES || life.getSickness() == Sickness.DIARRHEAL) {
+            return sicknessMultiplier = 1.2;
+        } else if (life.getSickness() == Sickness.PULMONARY || life.getSickness() == Sickness.DEMENTIAS) {
             return sicknessMultiplier = 1.3;
-        }
-        else if(life.getSickness()==Sickness.STROKE || life.getSickness()==Sickness.TUBERCULOSIS) {
+        } else if (life.getSickness() == Sickness.STROKE || life.getSickness() == Sickness.TUBERCULOSIS) {
             return sicknessMultiplier = 1.4;
-        }
-        else if(life.getSickness()==Sickness.CIRRHOSIS || life.getSickness()==Sickness.HEART) {
+        } else if (life.getSickness() == Sickness.CIRRHOSIS || life.getSickness() == Sickness.HEART) {
             return sicknessMultiplier = 1.5;
-        }
-        else if(life.getSickness()==Sickness.CANCER){
-            return  sicknessMultiplier = 2;
-        }
-
-
+        } else if (life.getSickness() == Sickness.CANCER) {
+            return sicknessMultiplier = 2;
+        } else return 0;
     }
+
     public double findAmateurSportMultiplier(Life life) {
         if (life.isAmateurSport()) {
             return amateurSportMultiplier = 1.2;
-        }
-        else return amateurSportMultiplier =1;
+        } else return amateurSportMultiplier = 1;
     }
 
     public double findAgeMultiplier(Life life) {
-        if (life.getAge()>50 && life.getAge()<60) {
-            return  ageMultiplier = 1.1;
-        }
-        else if( life.getAge()>60 && life.getAge()<70){
+        if (life.getAge() > 50 && life.getAge() < 60) {
+            return ageMultiplier = 1.1;
+        } else if (life.getAge() > 60 && life.getAge() < 70) {
             return ageMultiplier = 1.2;
-        }
-        else if (life.getAge()>70 && life.getAge()<100) {
+        } else if (life.getAge() > 70 && life.getAge() < 100) {
             return ageMultiplier = 1.4;
-        }
-        else if(life.getAge()>0 && life.getAge()<50) {
+        } else if (life.getAge() > 0 && life.getAge() < 50) {
             return ageMultiplier = 1;
-        }
-        else return 0;
+        } else return 0;
     }
+
     public double findBodyMultiplier(Life life) {
         double bodyVariable = life.getHeight() - life.getWeight();
         if (bodyVariable > 80 && bodyVariable < 110) {
@@ -93,11 +122,9 @@ public class LifeInsuranceService {
             return bodyMultiplier = 1.2;
         } else if (bodyVariable > 120 && bodyVariable < 130) {
             return bodyMultiplier = 1.3;
-        }
-        else if(bodyVariable<60 || bodyVariable >130) {
+        } else if (bodyVariable < 60 || bodyVariable > 130) {
             return bodyMultiplier = 1.5;
-        }
-        else return 0;
+        } else return 0;
 
     }
 
